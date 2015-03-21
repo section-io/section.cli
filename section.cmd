67f6250b014e1332ffc602a7374f435357e2dac5
Jason Stangroome <jason@squixa.com>
2015-03-22 08:16:28 +1100

    Recommend Vagrant be installed to use `section` and only suggest Ruby as a fallback.

--- section.cmd
+++ section.cmd
@@ -6,7 +6,10 @@ where /q %rubyexe% 2>NUL >NUL && goto foundruby
 
 for /f "delims=" %%A in ('where vagrant.exe') do set rubyexe=%%~dpA..\embedded\bin\ruby.exe
 if not exist "%rubyexe%" (
-  echo Ruby must be installed and on the PATH. Ruby can be downloaded at http://rubyinstaller.org/
+  echo Vagrant should be installed and on the PATH.
+  echo Vagrant can be downloaded at https://www.vagrantup.com/
+  echo Alternatively, for limited scenarios, Ruby should be installed and on the PATH.
+  echo Ruby can be downloaded at http://rubyinstaller.org/
   exit /b 1
 )
 
